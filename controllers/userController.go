615f21128bec11747f311e59f1d878fd90b1e78c
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-04-08 15:35:25 +0100

    pagination for users

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"math"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,11 +10,25 @@ import (
 )
 
 func Users(c *fiber.Ctx) error {
+	var total int64
+	p, _ := strconv.Atoi(c.Query("page", "1"))
+	l, _ := strconv.Atoi(c.Query("limit", "5"))
+
+	o := (p - 1) * l
 	su := []models.User{}
 
-	database.DB.Preload("Role").Find(&su)
+	database.DB.Preload("Role").Offset(o).Limit(l).Find(&su)
 
-	return c.JSON(su)
+	database.DB.Model(&models.User{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      p,
+			"last_page": math.Ceil(float64(total) / float64(l)),
+		},
+		"data": su,
+	})
 }
 
 func CreateUser(c *fiber.Ctx) error {
@@ -24,6 +39,8 @@ func CreateUser(c *fiber.Ctx) error {
 	}
 
 	u.SetPassword("12345")
+	//
+	// u.RoleId = 1
 
 	database.DB.Create(&u)
 
